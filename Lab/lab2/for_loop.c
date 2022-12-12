cfc884ef7d009fcebf98cbc92889a0613cff2ffe
ShoiLyaung <2596985510@qq.com>
2022-12-13 03:00:53 +0800

    modified:   Lab/lab2/for_loop.c

--- Lab/lab2/for_loop.c
+++ Lab/lab2/for_loop.c
@@ -9,11 +9,15 @@ int main()
 	int a = 50;
 	int b = 0;
 
-	#pragma omp parallel for default(shared)
+	//#pragma omp parallel for default(shared)
+	#pragma omp parallel for default(none) private(i) private(a) private(b)
+	//#pragma omp parallel for default(none) private(i) private(a) lastprivate(b)
+	//#pragma omp parallel for default(none) private(i) firstprivate(a) lastprivate(b)
 	for (i=0; i<N; i++)
 	{
 		b = a + i;
+		printf("a=%d\n", a);
 	}
 
-	printf("a=%d b=%d (expected a=50 b=1049)\n", a, b);
+	printf("i=%d\na=%d b=%d (expected a=50 b=1049)\n", i, a, b);
 }
